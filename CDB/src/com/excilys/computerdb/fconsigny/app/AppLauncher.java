7b3e632419bb286996cfed56bd4c997b2b1e50d0
fcAllSmith <fconsigny@excilys.com>
2017-03-01 19:22:05 +0100

    added packages services & exceptions & utils

--- CDB/src/com/excilys/computerdb/fconsigny/app/AppLauncher.java
+++ CDB/src/com/excilys/computerdb/fconsigny/app/AppLauncher.java
@@ -1,17 +1,52 @@
 package com.excilys.computerdb.fconsigny.app;
 
-import com.excilys.computerdb.fconsigny.dao.CompanyDao;
-import com.excilys.computerdb.fconsigny.dao.ComputerDao;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class AppLauncher {
 
 	public static void main(String[] args) {
-		System.out.println("-- Welcom  to Computer Database");
-		ComputerDao computerDao = new ComputerDao ();
-		computerDao.querySelectAll();
-		computerDao.querySelectById(50);
-		CompanyDao companyDao = new CompanyDao();
-		companyDao.querySelectAll();
-		companyDao.querySelectById(10);
+		showText("-- Welcom  to Computer Database");
+		showText("1 - Select all companies");
+		showText("2 - Select company with an id");
+		showText("3 - Select all computers");
+		showText("4 - Select a computer with an id");
+		
+		userCommand(getUserCommand());
+	}
+	
+	public static void userCommand(String strInput){
+		int cmd = Integer.parseInt(strInput); 
+		switch(cmd){
+		case 1:
+			break; 
+		case 2: 
+			break; 
+		case 3:
+			break;
+		case 4: 
+			break;
+		default:
+			
+			break; 
+		}
+	}
+	
+	public static String getUserCommand(){
+		String inputText = null; 
+		BufferedReader br = new BufferedReader(new InputStream(System.in));
+		try {
+			inputText = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return inputText;
+	}
+	
+	public static void showText(String strToPrint){
+		System.out.println(strToPrint);
 	}
 }
