343037f21d992af9cdc2cf21a8daaf309794fe1d
fcAllSmith <fconsigny@excilys.com>
2017-03-01 20:32:13 +0100

    Handle Computer Exceptions with the class CompanyException

---
+++ CDB/src/com/excilys/computerdb/fconsigny/services/CompanyException.java
@@ -0,0 +1,10 @@
+package com.excilys.computerdb.fconsigny.services;
+
+public class CompanyException extends Exception {
+
+	private static final long serialVersionUid = 1L;
+	public CompanyException(String message) {
+		super(message);
+	}
+
+}
