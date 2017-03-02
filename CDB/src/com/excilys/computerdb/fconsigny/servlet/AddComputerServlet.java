d16d8edd6bf73c81dcdf3ff10d7b43fc339eb5ae
fcAllSmith <fconsigny@excilys.com>
2017-03-02 06:48:16 +0100

    added Servlet DashboardServlet.Java and AddComputerSerlvet to test JdbcFilter is working

--- CDB/src/com/excilys/computerdb/fconsigny/servlet/AddComputerServlet.java
+++ CDB/src/com/excilys/computerdb/fconsigny/servlet/AddComputerServlet.java
@@ -1,9 +1,49 @@
 package com.excilys.computerdb.fconsigny.servlet;
 
-public class AddComputerServlet {
+import java.util.List;
 
-	public AddComputerServlet() {
-		// TODO Auto-generated constructor stub
+import com.excilys.computerdb.fconsigny.model.Company;
+import com.excilys.computerdb.fconsigny.services.CompanyServices;
+
+@WebServlet(urlPatterns = { "/computer"})
+public class AddComputerServlet extends HttpServlet{
+
+	private List<Company> companyList;
+
+	/**
+	 * Default serial VersionId.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		companyList = loadCompany();
+		//Set list into the view
+		request.setAttribute("companyList", companyList);
+		RequestDispatcher dispatcher = this.getServletContext()
+				.getRequestDispatcher("/WEB-INF/views/AddComputerFile.jsp");
+
+		dispatcher.forward(request, response);
 	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	/**
+	 *  load a company list in order to print it in a list 
+	 * @return List of companies we want to be sure that the computer added 
+	 *     is affected to a company which exist.
+	 */
+	public List<Company> loadCompany() {
+		//company listLoader
+		return new CompanyServices().getAllCompanies();
+	}
+
+}
 
 }
--- CDB/src/com/excilys/computerdb/fconsigny/servlet/DashboardServlet.java
+++ CDB/src/com/excilys/computerdb/fconsigny/servlet/DashboardServlet.java
@@ -2,8 +2,52 @@ package com.excilys.computerdb.fconsigny.servlet;
 
 public class DashboardServlet {
 
-	public DashboardServlet() {
-		// TODO Auto-generated constructor stub
-	}
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.excilys.computerdb.fconsigny.model.Computer;
+import com.excilys.computerdb.fconsigny.services.ComputerServices;
+
+@WebServlet(urlPatterns = { "/dashboard"})
+public class DashboardServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  public ViewDashboardServlet() {
+    super();
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    String username = request.getParameter("login");
+    request.setAttribute("username", username);
+
+    List<Computer> computerList = new ComputerServices().getAllComputers();
+    request.setAttribute("computerList", computerList);
+
+    RequestDispatcher dispatcher = this.getServletContext()
+        .getRequestDispatcher("/WEB-INF/views/Dashboard.jsp");
+    dispatcher.forward(request, response);
+
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+  
+    doGet(request, response);
+
+  }
+
 
 }
