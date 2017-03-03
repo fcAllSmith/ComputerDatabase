59a34208d3e446cf05b7daa4c6f17104bbe139cb
Consigny Francois <fconsigny@excilys.com>
2017-03-03 10:22:44 +0100

    Add - Package dto with the class computer dto

---
+++ CDB/src/com/excilys/computerdb/fconsigny/dto/ComputerDto.java
@@ -0,0 +1,12 @@
+package com.excilys.computerdb.fconsigny.dto;
+
+public class ComputerDto {
+	
+	private int id; 
+	private String name;
+	private String cdate; 
+	private String ndate; 
+	private int companyId;
+	
+	
+}
