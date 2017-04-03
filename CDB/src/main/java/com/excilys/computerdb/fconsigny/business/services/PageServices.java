4dc286e29eaa84186df00748453535f4b242acc6
Consigny Francois <fconsigny@excilys.com>
2017-04-03 11:31:02 +0200

    Update - Add transactional annotation into PageServices

--- CDB/src/main/java/com/excilys/computerdb/fconsigny/business/services/PageServices.java
+++ CDB/src/main/java/com/excilys/computerdb/fconsigny/business/services/PageServices.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.excilys.computerdb.fconsigny.business.exception.ServiceException;
 import com.excilys.computerdb.fconsigny.business.factory.ComputerFactory;
@@ -15,6 +16,7 @@ import com.excilys.computerdb.fconsigny.storage.dao.ComputerDao;
 import com.excilys.computerdb.fconsigny.storage.exceptions.DatabaseException;
 
 @Service("pageService")
+@Transactional
 public class PageServices implements IPageServices {
 
   private final ComputerDao computerDao = ComputerFactory.getComputerDao();
