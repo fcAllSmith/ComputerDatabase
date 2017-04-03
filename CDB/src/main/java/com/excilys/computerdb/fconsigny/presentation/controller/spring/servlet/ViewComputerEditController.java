6f39227af59955b1a20e4d5162986dacaed5f3e2
Consigny Francois <fconsigny@excilys.com>
2017-04-03 09:53:01 +0200

    Update - Spring for view dashboard

--- CDB/src/main/java/com/excilys/computerdb/fconsigny/presentation/controller/spring/servlet/ViewComputerEditController.java
+++ CDB/src/main/java/com/excilys/computerdb/fconsigny/presentation/controller/spring/servlet/ViewComputerEditController.java
@@ -1,7 +1,5 @@
 package com.excilys.computerdb.fconsigny.presentation.controller.spring.servlet;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
