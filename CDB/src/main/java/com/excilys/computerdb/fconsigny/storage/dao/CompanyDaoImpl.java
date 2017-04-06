35473c4fe723ef71f4a8b4c76e263761344fb4e0
Consigny Francois <fconsigny@excilys.com>
2017-04-06 10:45:29 +0200

    Update - CompanyDao working fully with Hibernate

--- CDB/src/main/java/com/excilys/computerdb/fconsigny/storage/dao/CompanyDaoImpl.java
+++ CDB/src/main/java/com/excilys/computerdb/fconsigny/storage/dao/CompanyDaoImpl.java
@@ -1,46 +1,35 @@
 package com.excilys.computerdb.fconsigny.storage.dao;
 
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
-import org.hibernate.transform.Transformers;
 import org.springframework.stereotype.Repository;
 
 import com.excilys.computerdb.fconsigny.storage.entity.EntityCompany;
+import com.excilys.computerdb.fconsigny.utils.property.FilePropertyLoader;
 
 @Repository
 public class CompanyDaoImpl implements CompanyDao {
 
   private static final String PROPERTY_FILE = "company.properties";
-  private static final String COL_ID = "id";
-  private static final String COL_NAME = "name";
-
-  private static Logger logger = Logger.getLogger(CompanyDaoImpl.class);
 
   @Override
   public List<EntityCompany> findAll(Session session) {
-    
-   /* Properties properties = FilePropertyLoader.loadSqlProperties(CompanyDaoImpl.class, PROPERTY_FILE);
-    String query = properties.getProperty("SELECT_ALL");
-    return (List<Company>) jdbc.queryForObject(query,  new BeanPropertyRowMapper(Company.class)); */
-
-    Query<EntityCompany> query  = session.createQuery("from " + EntityCompany.class.getName(), EntityCompany.class);
+    Properties properties = FilePropertyLoader.loadSqlProperties(CompanyDaoImpl.class, PROPERTY_FILE);
+    String str_query = properties.getProperty("SELECT_ALL");
+    Query<EntityCompany> query  = session.createQuery(str_query, EntityCompany.class);
     return (List<EntityCompany>) query.getResultList();
-    //Transaction t = session.beginTransaction() ; 
   }
 
   @Override
   public EntityCompany findById(Session session, final long id) {
-    
-   /* Properties properties = FilePropertyLoader.loadSqlProperties(CompanyDaoImpl.class, PROPERTY_FILE);
-    String query = properties.getProperty("SELECT_BY_ID");
-    return (Company) jdbc.queryForObject(query,  new BeanPropertyRowMapper(Company.class));*/
-    
-    org.hibernate.Query query = session.createQuery("select id,nale from company where id = :id "); 
-    query.setParameter(":id", id);
-    return (EntityCompany) ((Query) query).getSingleResult();
+    Properties properties = FilePropertyLoader.loadSqlProperties(CompanyDaoImpl.class, PROPERTY_FILE);
+    String str_query = properties.getProperty("SELECT_BY_ID");
+    Query<EntityCompany> query = session.createQuery(str_query,EntityCompany.class).setParameter("id", ((Number) id).intValue());
+    return (EntityCompany) query.getSingleResult();
   }
   
 }
